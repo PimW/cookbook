4e438d6a2ba0410ba6f1283874f9133cb2624ef6
pim <pim.wijn@gmail.com>
2022-04-18 18:49:36 +0200

    Working cooklang parser

--- src/cookbook/core.clj
+++ src/cookbook/core.clj
@@ -1,11 +1,27 @@
 (ns cookbook.core
+  "Cooklang parsing
+
+  A cooklang file is formatted according to:
+
+  The internal representation of a cooklang recipe is
+  of the format:
+  > Recipe
+    > metadata {}
+       > key: value
+    > steps []
+       > step []
+          > text
+          > comment
+          > ingredient
+          > cookware
+          > timer"
   (:require [clojure.java.io :as io]
             [clojure.string :as str]))
 
 
-(def comment-regex (re-pattern #"--\s*(.*)$"))
+(def comment-regex (re-pattern #"^\s*--\s*(.*)$"))
 (def metadata-regex (re-pattern #"^>>\s*(?<key>.+?):\s*(?<value>.+)"))
-(def time-regex (re-pattern #"~([^#~@\{]+)?\{(?<time>[^\}]*)(?:%(?<unit>[^}]+?)?)\}"))
+(def time-regex (re-pattern #"^~([^#~@\{]+)?\{(?<time>[^\}]*?)(?:%(?<unit>[^}]+)?)?\}"))
 
 (def ingredient-regex
   "Matches ingredients of type:
@@ -14,7 +30,7 @@
   - @eggwhite{1%g}
   - @egg white{1}
   - @egg"
-  (re-pattern #"@([^#~@\{]+)(?:\{(?<amount>[^\}]+?)?(?:%(?<unit>[^}]+?)?)?\})|@([^\s#~@\{]+)"))
+  (re-pattern #"^@([^#~@\{]+)(?:\{(?<amount>[^\}]+?)?(?:%(?<unit>[^}]+?)?)?\})|@([^\s#~@\{]+)"))
 
 (def cookware-regex
   "Matches cookware of the type:
@@ -22,7 +38,7 @@
   - #pan{ }
   - #big pan{}
   - #pan"
-  (re-pattern #"#([^#~@\{]+)\{([^\}]*)\}|#([^\s#~@\{]+)"))
+  (re-pattern #"^#([^#~@\{]+)\{([^\}]*)\}|#([^\s#~@\{]+)"))
 
 (defn- print-if-not-nil [str]
   (if (not (nil? str))
@@ -57,27 +73,18 @@
     (map #(vector (mapper %) (first %)) items))
   )
 
-(defn extract-time [line]
-  (extract
-    time-regex
-    #(hash-map
-       :name (second %)
-       :amount (nth % 2 nil)
-       :unit (nth % 3 nil))
-    line))
-
 (defn extract-cookware [line]
   (extract
     cookware-regex
     #(hash-map
-       :cookware (or (second %) (nth % 3 nil))) ; 2nd for complex 4th for simple
+       :cookware (or (second %) (nth % 3 nil)))             ; 2nd for complex 4th for simple
     line))
 
 (defn extract-ingredients [line]
   (extract
     ingredient-regex
     #(hash-map
-       :ingredient (or (second %) (nth % 4 nil)) ; 2nd for complex 5th for simple
+       :ingredient (or (second %) (nth % 4 nil))            ; 2nd for complex 5th for simple
        :amount (nth % 2 nil)
        :unit (nth % 3 nil))
     line))
@@ -90,46 +97,101 @@
        :value (nth % 2 nil))
     line))
 
-(defn next-special-char [line]
-  (let [idx (apply min
-                   (filter
-                     #(not (nil? %))
-                     (map #(str/index-of line %) ["@" "#" "~" ">" "-"])))
-        character (nth line idx)]
-    [idx character]))
+(defn next-special-char
+  ""
+  [line]
+  (let [indices (filter
+                  #(not (nil? %))
+                  (map #(str/index-of line %) ["@" "#" "~" ">" "-"]))
+        idx (or (when (not-empty indices) (apply min indices))
+                nil)]
+    idx))
+
+(defn- item
+  ; TODO: non-matching regex
+  [str]
+  (let [c (first str)]
+    (cond
+      (= c \#) (let [[result & g] (re-find cookware-regex str)
+                     l (count result)
+                     m {:cookware (or (first g) (nth g 2 nil))}]
+                 [l m])
+      (= c \@) (let [[result & g] (re-find ingredient-regex str)
+                     l (count result)
+                     m {:ingredient (or (first g) (nth g 3 nil)) ; 2nd for complex 5th for simple
+                        :amount     (second g)
+                        :unit       (nth g 2 nil)}]
+                 [l m])
+      (= c \~) (let [[result & g] (re-find time-regex str)
+                     l (count result)
+                     m {:name   (first g)
+                        :amount (nth g 1 nil)
+                        :unit   (nth g 2 nil)}]
+                 [l m])
+      ;(= c ">")
+      (= c \-) (let [[result & g] (re-find comment-regex str)
+                      l (count result)
+                      m {:comment (first g)}]
+                  [l m])
+      )))
 
 
-
-(defn parse-line [line]
+(defn parse-step [line]
+  (println line)
   (loop [l line
          parsed []]
-    (let [[idx character] (next-special-char line)
-          text (subs line 0 idx)
-          parsed (conj parsed {:text text})] ; add text before token
-      ; add special value
-      (recur (subs line idx) parsed)) ; TODO update idx
-    ))
-;  ; tokenized-line = []
-;  ; while find-next-token
-;  ;  put text in tokenized line
-;  ;  put token in tokenized line
-;  (loop []
-;    (re-find ))
-;  )
+    (let [idx (next-special-char l)]
+      (cond
+        (empty? l) parsed
+        (nil? idx) (conj parsed {:text l})
+        :else (let [text (subs l 0 idx)
+                    parsed (if (not-empty text)
+                             (conj parsed {:text text})
+                             parsed)
+                    [length i] (item (subs l idx))
+                    parsed (conj parsed i)]
+                (recur (subs l (+ idx length)) parsed))
+        )
+      )))
 
-(defn parse-old [line]
-  (extract-metadata line)
-  (extract-comments line)
-  (doseq [ingredient (extract-ingredients line)]
-    (println "-i " (ingredient-str ingredient)))
-  (doseq [cookware (extract-cookware line)]
-    (println "-c " (cookware-str cookware)))
-  (doseq [time (extract-time line)]
-    (println "-t " (time-str time))))
+; TODO: generic formatter?
+(defn step-to-cooklang [parsed-step]
+  (let [text-to-cl #(:text %)
+        cookware-to-cl #(str "#" (:cookware %))
+        comment-to-cl #(str "-- " (:comment %))
+        timer-to-cl #(str "~" (:name %)
+                          "{" (:amount %) (when (not (nil? (:unit %))) (str "%" (:unit %))) "}")
+        ingredient-to-cl #(str "@" (:ingredient %)
+                               "{" (:amount %) (when (not (nil? (:unit %))) (str "%" (:unit %))) "}")
+        item-to-cl #(cond
+                      (contains? % :cookware) (cookware-to-cl %)
+                      (contains? % :text) (text-to-cl %)
+                      (contains? % :name) (timer-to-cl %)
+                      (contains? % :ingredient) (ingredient-to-cl %)
+                      (contains? % :ingredient) (ingredient-to-cl %)
+                      (contains? % :comment) (comment-to-cl %)
+                      :else ""
+                      )]
+    (reduce str (map item-to-cl parsed-step))))
+
+(defn parse-line
+  "Parse a single line in a recipe.
+  Depending on the start of the line it is parsed as:
+   - Metadata
+   - Step
+
+  Empty lines are ignored"
+  [line]
+  (cond
+    (str/blank? line) nil
+    (str/starts-with? line ">>") nil
+    :else (parse-step line)))
 
 (defn parse-cooklang-file
   "docstring"
   [filename]
   (with-open [rdr (io/reader filename)]
-    (doseq [line (line-seq rdr)]
-      (parse-line line))))
+    (reduce conj [] (filter
+                      #(not (nil? %))
+                      (map parse-line (line-seq rdr))))
+    ))
